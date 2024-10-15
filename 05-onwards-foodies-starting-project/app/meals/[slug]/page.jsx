e4465c15395a8aca2c6ccb538c005f5af1e9b7aa
Oscar Hermawan <oscar@softwareseni.com>
2024-10-15 17:04:20 +0700

    NextJS - foodies project

---
+++ 05-onwards-foodies-starting-project/app/meals/[slug]/page.jsx
@@ -0,0 +1,9 @@
+export default function MealsSLug() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Meals Slug
+      </h1>
+    </main>
+  );
+}
---
+++ 05-onwards-foodies-starting-project/app/meals/share/page.jsx
@@ -0,0 +1,9 @@
+export default function ShareMealsPage() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Share Meals Page
+      </h1>
+    </main>
+  );
+}
